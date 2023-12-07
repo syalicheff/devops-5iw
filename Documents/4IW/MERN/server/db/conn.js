ff5f5f67bdd64a991a4e9a8470534bedf6afcd14
syalicheff <70692749+syalicheff@users.noreply.github.com>
2023-12-07 11:55:49 +0100

    Delete Documents directory

--- Documents/4IW/MERN/server/db/conn.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose'
-
-export async function db_connect(){
-    return mongoose.connect(('mongodb+srv://App_Autoequip:Autoequip94@cluster0.kjbyh.mongodb.net/mern_app?retryWrites=true&w=majority'), {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-}
